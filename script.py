eb53241858e5fbdd22c34103d169756734035a39
Patrick Conrey <patrickconrey@gmail.com>
2018-02-20 16:15:42 -0500

    :rocket: sampling tests for 1kHz

--- script.py
+++ script.py
@@ -3,17 +3,36 @@
 # Author: Tony DiCola
 # License: Public Domain
 import time
-
-# Import SPI library (for hardware SPI) and MCP3008 library.
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
+from threading import Timer
+
+
+Ts = 1 / 1000
+shouldContinue = True
+samples = []
 
 # Hardware SPI configuration:
-SPI_PORT   = 0
+SPI_PORT = 0
 SPI_DEVICE = 0
 mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
+def invalidate_timer():
+    shouldContinue = False
+    print("Suspected Samples: " + str(1/Ts))
+    print("Actual Samples: " + str(len(samples)))
+    if (1/Ts) == len(samples):
+        print("🔥🔥🔥🔥🔥🔥🔥🔥")
+    elif float(len(samples)) > 0.9*(1/Ts):
+        print("🙁🙁🙁🙁🙁🙁🙁🙁")
+    else:
+        print("😢😢😢😢😢😢😢😢")
+
+
+t = Timer(Ts, invalidate_timer)
+t.start()
+
 print('Reading MCP3008 values, press Ctrl-C to quit...')
-while True:
-    print(mcp.read_adc(0)*'-'+"*#*")
-    time.sleep(0.5)
+while shouldContinue:
+    samples.append(mcp.read_adc(0))
+    time.sleep(Ts)
