5ed70082e4e378176306290252567d1ec2628581
Patrick Conrey <patrickconrey@gmail.com>
2018-02-20 15:46:39 -0500

    :sparkles: Actual code

--- script.py
+++ script.py
@@ -1,1 +1,19 @@
-print("hello, world!")
+# Simple example of reading the MCP3008 analog input channels and printing
+# them all out.
+# Author: Tony DiCola
+# License: Public Domain
+import time
+
+# Import SPI library (for hardware SPI) and MCP3008 library.
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+
+# Hardware SPI configuration:
+SPI_PORT   = 0
+SPI_DEVICE = 0
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+
+print('Reading MCP3008 values, press Ctrl-C to quit...')
+while True:
+    print(mcp.read_adc(0)*'-'+"*#*")
+    time.sleep(0.5)
