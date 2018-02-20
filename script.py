3650c4b01412698d73025ea4a7d7e46ebc703660
Patrick Conrey <patrickconrey@gmail.com>
2018-02-20 16:20:24 -0500

    :ambulence: Fixing int rounding :/

--- script.py
+++ script.py
@@ -19,14 +19,14 @@ mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
 def invalidate_timer():
     shouldContinue = False
-    print("Suspected Samples: " + str(1/Ts))
-    print("Actual Samples: " + str(len(samples)))
+    print("Suspected Samples: " + repr(1/Ts))
+    print("Actual Samples: " + repr(len(samples)))
     if (1/Ts) == len(samples):
-        print("Perfect Performance | Sampling: " + str(len(samples)) + " Out of: " + str(1 / Ts))
+        print("Perfect Performance | Sampling: " + repr(len(samples)) + " Out of: " + repr(1 / Ts))
     elif float(len(samples)) > 0.9*(1/Ts):
-        print("Alright Performance | Sampling: " + str(len(samples)) + " Out of: " + str(1 / Ts))
+        print("Alright Performance | Sampling: " + repr(len(samples)) + " Out of: " + repr(1 / Ts))
     else:
-        print("Very Poor Performance | Sampling: " + str(len(samples)) + " Out of: " + str(1/Ts))
+        print("Very Poor Performance | Sampling: " + repr(len(samples)) + " Out of: " + repr(1/Ts))
 
 
 t = Timer(Ts, invalidate_timer)
