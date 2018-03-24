e558e34de9fc6ead53210d27b2507752f7cda2b3
Patrick Conrey <patrickconrey@gmail.com>
2018-03-24 15:08:54 -0400

    whoops - silly typo pushed

--- server.py
+++ server.py
@@ -36,9 +36,6 @@ SPI_DEVICE = 0
 mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
 
-def beginSamplingGlobal(protocol, isBinary):
-
-
 class MyServerProtocol(WebSocketServerProtocol):
 
     def onConnect(self, request):
