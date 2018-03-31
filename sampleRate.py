e3e3ebf41c8cb8995d6a784034a410b81c27dd3f
Patrick Conrey <patrickconrey@gmail.com>
2018-03-31 17:37:47 -0400

    more samples, better res

--- sampleRate.py
+++ sampleRate.py
@@ -3,6 +3,7 @@ import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 
 samples = []
+NUM_SAMPLES = 100000
 
 # Hardware SPI configuration:
 SPI_PORT = 0
@@ -11,8 +12,8 @@ mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
 start = datetime.datetime.now()
 
-for x in range(0, 1000):
+for x in range(0, NUM_SAMPLES):
     samples.append(x)
 
 end = datetime.datetime.now()
-print("Sample Rate: " + str(1000 / (end.second - start.second)))
+print("Sample Rate: " + str(NUM_SAMPLES / (end.second - start.second)))
