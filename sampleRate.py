4fe248c3c84a4999253f34ada3416eacf0524381
Patrick Conrey <patrickconrey@gmail.com>
2018-03-31 17:35:10 -0400

    added a sample frequency test

---
+++ sampleRate.py
@@ -0,0 +1,15 @@
+import datetime
+samples = []
+
+# Hardware SPI configuration:
+SPI_PORT = 0
+SPI_DEVICE = 0
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+
+start = datetime.datetime.now()
+
+for x in range(0, 1000):
+    samples.append(x)
+
+end = datetime.datetime.now()
+print("Sample Rate: " + str(1000 / (end.second - start.second)))
