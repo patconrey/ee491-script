4615844744e8964f54960cdf7a7207fce89ae495
Patrick Conrey <patrickconrey@gmail.com>
2018-04-03 13:39:13 -0400

    debug

--- server.py
+++ server.py
@@ -50,6 +50,7 @@ class MyServerProtocol(WebSocketServerProtocol):
         # Grab 0.025 [s] of data based on sample rate of 8 [kHz]
         for x in range(0, 200 - 1):
             buffer.append((mcp.read_adc(0) - 512) / 512.0)
+            print((mcp.read_adc(0)-512) / 512.0)
             time.sleep(0.000125)
         self.sendMessage(str(buffer), isBinary)
 
